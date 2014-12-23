a78c5ce55b5bee5f1695f81658d3423de83f667a
Kagami Hiiragi <kagami@genshiken.org>
2014-12-23 23:40:27 +0300

    Delete old code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import "traceur/bin/traceur-runtime";
-import th from "telehash-promise";
-import tpws from "telehash-ws";
 import * as db from "./db";
 import * as util from "./util";
-
-function initTelehash() {
-  // XXX(Kagami): See <https://github.com/telehash/telehash-js/issues/23>.
-  delete th.extensions.udp4;
-  delete th.extensions.tcp4;
-  delete th.extensions.http;
-  th.add(tpws);
-}
 
 export function start() {
   util.initConfig();
   util.initLog(th);
-  initTelehash();
   db.initDb();
 }
 
