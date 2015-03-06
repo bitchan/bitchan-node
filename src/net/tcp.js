af5d6a5989e5e10e8e8d06964e915dec8d370d44
Kagami Hiiragi <kagami@genshiken.org>
2015-03-06 16:23:33 +0300

    Refactor tcp module (Map, const, logging)

--- src/net/tcp.js
+++ src/net/tcp.js
@@ -11,10 +11,10 @@ import conf from "../config";
 import * as storage from "../storage";
 import {DEFAULT_STREAM, MY_USER_AGENT, getLogger} from "./common";
 import * as knownNodes from "./known-nodes";
-import {objectValues} from "../util";
 
 const messages = bitmessage.messages;
 const ServicesBitfield = bitmessage.structs.ServicesBitfield;
+const logSilly = getLogger("TCP", "silly");
 const logDebug = getLogger("TCP", "debug");
 const logInfo = getLogger("TCP", "info");
 const logWarn = getLogger("TCP", "warn");
@@ -61,15 +61,10 @@ function getTrustedPeer(cfgTrustedPeer, stream) {
   });
 }
 
-// Hash of connected/half-open transports accessed by IP.
-const connected = Object.create(null);
+// Dictionary of connected/half-open transports accessed by IP.
+const connected = new Map();
 // Number of outcoming connections.
 let outcomingNum = 0;
-
-// Return whether the given host is already connected/half-open.
-function isConnected(host) {
-  return Object.prototype.hasOwnProperty.call(connected, host);
-}
 
 // Return limit of outcoming connections.
 function getOutcomingLimit(trustedPeer) {
@@ -82,7 +77,7 @@ function getNode({stream, trustedPeer}) {
   if (trustedPeer) {
     return Promise.resolve(trustedPeer);
   } else {
-    let exclude = Object.keys(connected);
+    let exclude = [...connected.keys()];
     return knownNodes.getRandom(stream, exclude);
   }
 }
@@ -109,7 +104,7 @@ function runOutcomingLoop(opts) {
     // host once more time (`getNode` also filters connected nodes) to
     // protect ourself against race condition because of async SQL
     // query.
-    if (!isConnected(host)) {
+    if (!connected.has(host)) {
       logInfo("Connecting to %s:%s", host, port);
       let transport = createTransport({stream: opts.stream, port: opts.port});
       outcomingNum++;
@@ -129,11 +124,11 @@ function runOutcomingLoop(opts) {
 }
 
 function listenIncoming(opts) {
-  let server = createTransport({stream: opts.stream, port: opts.port});
+  const server = createTransport({stream: opts.stream, port: opts.port});
   // TODO(Kagami): Pass new connection info as an object?
   server.on("connection", function(transport, host, port) {
     logInfo("Got new connection from %s:%s", host, port);
-    if (isConnected(host)) {
+    if (connected.has(host)) {
       logInfo("We are already connected to %s, closing connection", host);
       transport.close();
       return;
@@ -151,16 +146,10 @@ function listenIncoming(opts) {
   logInfo("Listening at %s:%s", opts.host, opts.port);
 }
 
-// Report info about current connections.
-function logConnInfo() {
-  let allNum = Object.keys(connected).length;
-  logDebug("Total %s connection(s) (%s outcoming)", allNum, outcomingNum);
-}
-
 // Human-readably size of the message.
 function getSize(payload) {
   // Message header length is 24 bytes.
-  let len = payload.length + 24;
+  const len = payload.length + 24;
   if (len >= 1024) {
     return (len / 1024).toFixed(2) + "KiB";
   } else {
@@ -170,9 +159,11 @@ function getSize(payload) {
 
 // Setup event handlers for a new incoming/outcoming connection.
 function initTransport({transport, host, port, stream}) {
-  let start = new Date().getTime();
-  connected[host] = transport;
-  logConnInfo();
+  let start = moment();
+  connected.set(host, transport);
+  logDebug(
+    "Total %s connection(s) (%s outcoming)",
+    connected.size, outcomingNum);
 
   // Don't update this particular transport too often.
   let lastUpdate = moment(0);
@@ -184,7 +175,7 @@ function initTransport({transport, host, port, stream}) {
   }
 
   transport.on("established", function(version) {
-    let delta = (new Date().getTime() - start) / 1000;
+    let delta = moment().diff(start, "seconds", true);
     logInfo(
       "Connection to %s:%s (%s) was established in %ss",
       host, port, version.userAgent, delta);
@@ -198,12 +189,12 @@ function initTransport({transport, host, port, stream}) {
     sendBigInv({transport, stream});
 
     transport.on("message", function(command, payload) {
-      let start = new Date().getTime();
+      start = moment();
       bumpActivity();
-      logDebug(
+      logSilly(
         "Got new message '%s' (%s) from %s",
         command, getSize(payload), transport);
-      let handler = messageHandlers[command];
+      const handler = messageHandlers[command];
       if (!handler) {
         return logWarn("Skip unknown message '%s' from %s", command, transport);
       }
@@ -218,8 +209,8 @@ function initTransport({transport, host, port, stream}) {
           command, transport, err.message);
       }
 
-      let delta = (new Date().getTime() - start) / 1000;
-      logDebug(
+      delta = moment().diff(start, "seconds", true);
+      logSilly(
         "Message '%s' from %s was successfully processed in %ss",
         command, transport, delta);
     });
@@ -235,17 +226,19 @@ function initTransport({transport, host, port, stream}) {
 
   transport.on("close", function() {
     logInfo("Connection to %s:%s was closed", host, port);
-    delete connected[host];
-    logConnInfo();
+    connected.delete(host);
+    logDebug(
+      "Total %s connection(s) (%s outcoming)",
+      connected.size, outcomingNum);
   });
 }
 
 // Broadcast given message to all connected transports.
 // FIXME(Kagami): Timing attack mitigation.
 function broadcast(...args) {
-  objectValues(connected).forEach(function(transport) {
-    transport.send(...args);
-  });
+  // XXX(Kagami): Better to use `for...of` but it seems to generate
+  // inefficient code in babel currently.
+  [...connected.values()].forEach(transport => transport.send(...args));
 }
 
 // NOTE(Kagami): We need hoisting to use this variable in
@@ -311,7 +304,7 @@ function sendBigAddr({transport, stream}) {
 
 function broadcastAddrs(addrs) {
   if (!addrs.length) { return; }
-  logDebug("Broadcast %s network address(es)", addrs.length);
+  logInfo("Broadcast %s network address(es)", addrs.length);
   const addr = messages.addr.encode(addrs);
   broadcast(addr);
 }
--- src/util.js
+++ src/util.js
@@ -13,9 +13,3 @@ exports.popkey = function(obj, key) {
   delete obj[key];
   return value;
 };
-
-exports.objectValues = function(obj) {
-  return Object.keys(obj).map(function(key) {
-    return obj[key];
-  });
-};
